ab935d62037e22ef2b72a7f3e388b8a591e3fc3f
JI-76 <johnison76@hotmail.com>
2021-04-27 15:55:54 -0700

    Project Setup

--- dropanddrag.js
+++ dropanddrag.js
@@ -1,3 +1,5 @@
+// dropanddrag.js
+
 const addBtns = document.querySelectorAll('.add-btn:not(.solid)');
 const saveItemBtns = document.querySelectorAll('.solid');
 const addItemContainers = document.querySelectorAll('.add-container');
